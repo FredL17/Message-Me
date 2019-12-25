83a7b0cf298ad0055ddc3cd6cdc4fdb261d0d719
Feifan17 <lin.2570@osu.edu>
2019-12-25 18:30:50 -0500

    create User and Message tables and login page

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
+gem 'bcrypt', '~> 3.1.7'
+gem 'hirb'
+
 gem 'jquery-rails'
 #Use semantic-UI for styling
 gem 'semantic-ui-sass'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'user1', password: '123456')
+User.create(username: 'user2', password: '123456')
+User.create(username: 'user3', password: '123456')
+User.create(username: 'user4', password: '123456')
+User.create(username: 'user5', password: '123456')
+
+Message.create(body: 'This is the first message!', user_id: 1)
+Message.create(body: 'This is the second message!', user_id: 2)
+Message.create(body: 'This is the third message!', user_id: 3)
+Message.create(body: 'This is the fourth message!', user_id: 4)
+Message.create(body: 'This is the fifth message!', user_id: 5)
